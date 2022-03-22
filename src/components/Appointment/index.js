ecf52c0a67676fa0583dec88b995a119b7d86426
scee10 <schhor05@gmail.com>
2022-03-22 19:50:57 -0400

    Set up confirm, error, show, status components and stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./styles.scss";
 import Header from './Header';
 import Empty from './Empty';
+import Show from './Show';
 
 export default function Appointment (props) {
 
