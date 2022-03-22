4361a8e1d779d8194b8f5429465511c2f4f99066
scee10 <schhor05@gmail.com>
2022-03-22 14:39:56 -0400

    Component: finish InterviewListItem using Storybook to test

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,13 +1,22 @@
-export default function DayListItem(props) {
-  
+import React from "react";
+import "components/InterviewListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewListItem(props) {
+  const { name, avatar, id, setInterviewer, selected, image } = props;
+  const nameClass = classNames("interviewers__item",{
+    "day-list__item--selected": selected,
+    "interviewers__item-image": !image
+  })
+
   return (
-    <li className="interviewers__item">
+    <li className={nameClass} onClick={() => {setInterviewer(id)}}>
       <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        className={nameClass}
+        src={avatar}
+        alt={name}
       />
-      Sylvia Palmer
+      {selected && name}  
     </li>
   );
 }
