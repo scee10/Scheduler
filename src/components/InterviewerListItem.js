0883e572d015bc83262fe0c41cd0d2bac51a7d5a
scee10 <schhor05@gmail.com>
2022-03-22 12:42:40 -0400

    Setup component structure: InterviewListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,13 @@
+export default function DayListItem(props) {
+  
+  return (
+    <li className="interviewers__item">
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  );
+}
