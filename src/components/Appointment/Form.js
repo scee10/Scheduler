5211408eaba654b3705b1556a8c1dc8e63c53a23
scee10 <schhor05@gmail.com>
2022-03-22 22:06:38 -0400

    Form component state updates in input

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,28 +1,28 @@
 import React from "react";
 import InterviewList from "components/InterviewList";
 import Button from "components/Button";
+import { useState } from 'react';
 
 export default function Form (props) {
   const {onCancel, onSave, interviewer, interviewers, student} = props;
-
+  const [studentName, setStudent] = useState(student || "");
+  const [interviewerID, setInterviewer] = useState(interviewer || null);
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
+            onChange={(event) => setStudent(event.target.value)}
             className="appointment__create-input text--semi-bold"
-            value={student}
+            value={studentName}
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
           />
         </form>
         <InterviewList 
           interviewers={interviewers}
           value={interviewer}
+          onChange={(interviewerID) => setInterviewer({interviewerID})}
         />
       </section>
       <section className="appointment__card-right">
