f6905e56d7b172ec12923f960bb6efe4da59fb4e
scee10 <schhor05@gmail.com>
2022-03-24 18:05:16 -0400

    Create helpers function to display interviewer names on browser

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,7 +7,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,24 +1,28 @@
 export function getAppointmentsForDay(state, day) {
-
   if (state.days.length === 0) {
     return [];
   };
-
   const correctDay = state.days.find(dayObj => {
     return dayObj.name === day;
   });
-
   if (correctDay === undefined) {
     return [];
   };
-
   const appointmentsForDay = correctDay.appointments.map(id => {
     return state.appointments[id];
   });
-
   return appointmentsForDay;
 }
 
 export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  };
+
+  let interviewerDetails = state.interviewers[interview.interviewer];
   
+  return {
+    student: interview.student,
+    interviewer: interviewerDetails
+  };
 }
