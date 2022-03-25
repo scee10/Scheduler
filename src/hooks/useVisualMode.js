f9b00f4dde8e362cee134bec808cd591c8dd2c64
scee10 <schhor05@gmail.com>
2022-03-24 20:03:54 -0400

    create useVisualMode custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+    function transition(newMode, replace = false) {
+      if (replace) {
+        setMode((prev) => newMode);
+        let replaceHistoryArr = [...history];
+        replaceHistoryArr[replaceHistoryArr.length - 1] = mode;
+        setHistory((prev) => replaceHistoryArr);
+      } else {
+        setMode((prev) => newMode);
+        let newHistoryArr = [...history];
+        newHistoryArr.push(newMode);
+        setHistory((prev) => newHistoryArr);
+      }
+    }
+
+    function back() {
+      let newHistoryArr = [...history];
+      newHistoryArr.pop(mode);
+      setHistory((prev) => newHistoryArr);
+      if (history.length > 1) {
+        setMode((prev) => newHistoryArr[newHistoryArr.length - 1]);
+      }
+    }
+
+  return { mode, transition, back };
+}
+
