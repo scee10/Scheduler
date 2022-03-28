d4cab525bf561e35c2a772b01e2e088e66ea3f4c
scee10 <schhor05@gmail.com>
2022-03-28 15:13:54 -0400

    Improve: Move application.js logic to useApplicationData.js file

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,59 +1,16 @@
 import React from "react";
-import axios from 'axios';
-
-import { useState, useEffect } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 
 export default function Application(props) {
-  const [state, setState] = useState ({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
 
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.put(`/api/appointments/${id}`,{interview})
-    .then(res => {
-      setState({
-      ...state,
-      appointments})}
-      );
-  }
-
-  function cancelInterview(id) {
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-    return axios.delete(`/api/appointments/${id}`,appointment)
-    .then(() => {
-      setState({...state, appointments})
-    })
-
-  }
-
-  const setDay = day => setState({ ...state, day });
+  const {state, setDay, bookInterview, cancelInterview} = useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
@@ -72,17 +29,6 @@ export default function Application(props) {
       />
     )
   })
-
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ])
-    .then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
-    })
-}, [])
 
   return (
     <main className="layout">
