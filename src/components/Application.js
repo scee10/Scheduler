dccf7e3f9af2320d337eec66fbaa506c4a03293f
scee10 <schhor05@gmail.com>
2022-03-28 14:18:05 -0400

    Feat: Delete interview

--- src/components/Application.js
+++ src/components/Application.js
@@ -24,20 +24,35 @@ export default function Application(props) {
       ...state.appointments[id],
       interview: { ...interview }
     };
-
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
-
     return axios.put(`/api/appointments/${id}`,{interview})
     .then(res => {
       setState({
       ...state,
       appointments})}
       );
+  }
+
+  function cancelInterview(id) {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.delete(`/api/appointments/${id}`,appointment)
+    .then(() => {
+      setState({...state, appointments})
+    })
 
   }
+
   const setDay = day => setState({ ...state, day });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -53,6 +68,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={dailyInterviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     )
   })
@@ -92,7 +108,11 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {appointmentsArr}
-        <Appointment time="5pm" bookInterview={bookInterview}/>
+        <Appointment 
+          time="5pm" 
+          bookInterview={bookInterview} 
+          cancelInterview={cancelInterview}
+        />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,49 +5,72 @@ import Empty from './Empty';
 import Show from './Show';
 import Form from './Form';
 import Status from './Status';
+import Confirm from './Confirm';
 import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment (props) {
-  const { time, interview, interviewers, bookInterview } = props;
+  const { time, interview, interviewers, bookInterview, id, cancelInterview } = props;
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
-  function save(name, interviewer) {
 
+  function save(name, interviewer) {
     if (name && interviewer) {
       transition(SAVING)
     }
-
       const interview = {
         student: name,
         interviewer
       };
-
-      bookInterview(props.id, interview)
+      bookInterview(id, interview)
       .then(() => transition(SHOW))
+  }
+
+  function removeInterview() {
+      transition(DELETING, true)
+      cancelInterview(id)
+        .then(() => 
+        transition(EMPTY))
+  }
+
+  function confirmCancelInt() {
+    transition(CONFIRM)
   }
 
   return (
     <article className="appointment">
       <Header 
         time={time}/>
-      {mode === EMPTY && <Empty 
-        onAdd={() => transition(CREATE)} />}
-      {mode === CREATE && <Form
-        interviewers={interviewers} 
-        onCancel={() => back(EMPTY)} 
-        onSave={save}/>}
+      {mode === EMPTY && 
+        <Empty 
+          onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && 
+        <Form
+          interviewers={interviewers} 
+          onCancel={() => back(EMPTY)} 
+          onSave={save}
+          />}
       {mode === SHOW && (
-      <Show
-        student={interview.student}
-        interviewer={interview.interviewer}
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+          onDelete={confirmCancelInt}
         />
       )}
+      {mode === CONFIRM && 
+        <Confirm 
+          onCancel={back}
+          onConfirm={removeInterview}
+          message={"Are you sure you would like to delete this interview?"}
+        />}
       {mode === SAVING && <Status message={"Saving"}/>}
+      {mode === DELETING && <Status message={"Deleting"}/>}
     </article>
   )
 }
