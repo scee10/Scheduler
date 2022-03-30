e5d377a3f010eaf90e01d8115d9b00dcd929ba99
scee10 <schhor05@gmail.com>
2022-03-30 11:05:52 -0400

    Cypress tests remaining now passing

--- cypress/integration/1-getting-started/appointment.spec.js
+++ cypress/integration/1-getting-started/appointment.spec.js
@@ -20,6 +20,38 @@ describe("Appointments", () => {
    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({force: true});
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia")
+
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+   });
+
+   it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+     .first()
+     .click({force: true})
+
+    cy.contains("Confirm")
+      .click()
+    
+    cy.contains("Deleting")
+    cy.contains("Deleting").should("not.exist")
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
+   });
  });
 
 
